eb2388b449dc666c26c1210dd444573b1cb8241d
Diana Espitia <dpaolac@gmail.com>
2023-02-20 20:04:51 -0500

    testing styles for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,6 +5,7 @@ const Card = ({ title, children, footer }) => {
   return (
     <div className="card">
       <h1>{title}</h1>
+      <h1 id="parrafo" className="titulo" style={{'color': 'yellow'}}>parrafo</h1>
       {children}
       <footer>
         {footer}
