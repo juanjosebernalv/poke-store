a98e9c5c0fdad34e460824b9e8ec7357d117a30b
Juan Jose Bernal Villamarin <juanjosebernalvillamarin@gmail.com>
2023-02-20 19:03:14 -0500

    Poke stored created ugly card

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React, { Children } from 'react';
+import '../styles/components.css';
+
+const Card = ({ title, children, footer }) => {
+  return (
+    <div className="card">
+      <h1>{title}</h1>
+      {children}
+      <footer>
+        {footer}
+      </footer>
+    </div>
+  );
+};
+
+export default Card;
