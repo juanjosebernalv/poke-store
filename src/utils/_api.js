153430d772021faf0da126d40a8cba6408ebeb22
Adriana Rozo <adrianarozo@gmail.com>
2023-02-27 18:53:18 -0500

    modificando api

--- src/utils/_api.js
+++ src/utils/_api.js
@@ -1,5 +1,5 @@
-export const queryPokemon = (pokemonName) => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+export const queryPokemon = (pokemonName) => 
+   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => (data));
-};
+;
