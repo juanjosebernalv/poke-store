35f5d9d56d26d47d36d4720daf61ef472c4a24e3
Adriana Rozo <adrianarozo@gmail.com>
2023-02-27 19:00:58 -0500

    ya funciona

--- src/App.js
+++ src/App.js
@@ -25,9 +25,21 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-
-      </header> */}
+      {/*
+      {pokemon && pokemon.sprites ?
+        <section>
+          <Card title="Component one" footer={<div>footer</div>}>
+            <img src={pokemon.sprites['front_default']} alt='poketest' />
+            <p>
+              Hello Pikachu
+            </p>
+            <h2>Si</h2>
+          </Card>
+        </section>
+        : <div>Loading...</div>
+      }
+      
+      */}
       <section>
         <Card title={pokemon.name} footer={<div>footer</div>}>
            <img src={pokemon.sprites['front_default']} alt='poketest' /> 
@@ -39,7 +51,6 @@ function App() {
           <div className="container">
             <h2 style={{'backgroundColor':'black'}}>dd</h2>
             <p>ddd</p>
-
           </div>
         </Card>
       </section>
